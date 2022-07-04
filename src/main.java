e5a429b055ced1236dbd4f9b20f5088e1efe795e
juan pablo gomez <Juangomezramirez527@gmil.com>
2022-07-04 15:22:48 -0500

    second try

--- src/main.java
+++ src/main.java
@@ -8,7 +8,10 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         Scanner entrada = new Scanner(System.in);
-        
+        String name;
+        System.out.println("Ingrese su nombre");
+        name=entrada.nextLine();
+        System.out.println("su nombre es: "+name);
     }
     
 }
