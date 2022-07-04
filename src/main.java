85f1ed4e394911ca8485db197da3d1d57d823b3f
juan pablo gomez <Juangomezramirez527@gmil.com>
2022-07-04 15:20:02 -0500

    primer intento

---
+++ src/main.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+
+public class main {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        Scanner entrada = new Scanner(System.in);
+        
+    }
+    
+}
